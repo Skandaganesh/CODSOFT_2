f9ec5a05694f86e126fdef2f4da8b5489206347b
Skandaganesh <49191239+Skandaganesh@users.noreply.github.com>
2023-11-05 21:17:32 +0530

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    char opr;
+    cout << "Enter first number\n";
+    cin >> num1;
+    cout << "Enter second number\n";
+    cin >> num2;
+    cout << "Enter operator(+,-,*,/)";
+    cin >> opr;
+    switch (opr)
+    {
+    case '+':
+        cout << "Sum of " << num1 << " and " << num2 << " is " << (num1 + num2);
+    case '-':
+        cout << "Difference of " << num1 << " and " << num2 << " is " << (num1 - num2);
+    case '*':
+        cout << "Product of " << num1 << " and " << num2 << " is " << (num1 * num2);
+    case '/':
+        cout << "Quotient of " << num1 << " and " << num2 << " is " << (num1 / num2);
+    }
+}
